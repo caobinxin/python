00e134b0faefb2d17400f402d649bbc66deab133
caobinxin <caobinxin666@163.com>
2019-06-04 23:37:34 +0800

    feat: 面向对象学习

---
+++ 00.廖雪峰教程/05.面向对象/02.继承和多态/animals.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+
+class Cat(Animal):
+    def run(self):
+        print('Cat is running...')
+
+# 对于静态语言（例如Java）来说，如果需要传入Animal类型，
+# 则传入的对象必须是Animal类型或者它的子类，否则，将无法调用run()方法。
+
+# 对于Python这样的动态语言来说，则不一定需要传入Animal类型。
+# 我们只需要保证传入的对象有一个run()方法就可以了：
+
+class Pig(): # 这里没有继承任何一个类
+    def run(self):
+        print('Pig is running...')
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+a = Animal()
+d = Dog()
+c = Cat()
+
+print('a is Animal?', isinstance(a, Animal))
+print('a is Dog?', isinstance(a, Dog))
+print('a is Cat?', isinstance(a, Cat))
+
+print('d is Animal?', isinstance(d, Animal))
+print('d is Dog?', isinstance(d, Dog))
+print('d is Cat?', isinstance(d, Cat))
+
+run_twice(c)
+
+# 仔细看这里的pig 类的定义，他并没有继承任何一个类，
+p = Pig()
+run_twice(p)
+
---
+++ 00.廖雪峰教程/05.面向对象/02.继承和多态/animals02.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Animal(object):
+    __name = 'Animal' # 私有成员
+    def run(self):
+        print('%s is running...' % self.__name)
+
+class Dog(Animal):
+    __name = 'Dog'
+    def run(self):
+        print('%s is running...' % self.__name)
+
+class Cat(Animal):
+    __name = 'Cat'
+    def run(self):
+        print('%s is running...' % self.__name)
+
+class Pig(): # 这里没有继承任何一个类
+    __name = 'Pig'
+    def run(self):
+        print('%s is running...' % self.__name)
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+a = Animal()
+d = Dog()
+c = Cat()
+
+print('a is Animal?', isinstance(a, Animal))
+print('a is Dog?', isinstance(a, Dog))
+print('a is Cat?', isinstance(a, Cat))
+
+print('d is Animal?', isinstance(d, Animal))
+print('d is Dog?', isinstance(d, Dog))
+print('d is Cat?', isinstance(d, Cat))
+
+run_twice(c) # 变量应该不能多态，多态的是 函数
+
+# 仔细看这里的pig 类的定义，他并没有继承任何一个类，
+p = Pig()
+run_twice(p)
+
